6a2750bb445948d261cbc4f34e606fb8d977dfbf
DeanAvram <dean6198@gmail.com>
2024-05-25 10:01:38 +0300

    Initial Commit

---
+++ src/main/java/cloud/user/reactiveusermanagementmicroservice/ReactiveUserManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package cloud.user.reactiveusermanagementmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveUserManagementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveUserManagementMicroserviceApplication.class, args);
+	}
+
+}
